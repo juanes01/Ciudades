a74d3f1d783191a4f2c60a1dd8167db176764cd5
Juan <juanes-x@hotmail.com>
2019-07-24 23:44:27 -0500

    Sexto commit para Heroku, se corrigen archivos estaticos5

--- Ciudades/settings.py
+++ Ciudades/settings.py
@@ -144,5 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
