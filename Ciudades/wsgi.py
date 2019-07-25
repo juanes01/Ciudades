d60b1ec4931dcd31a8c96631377a7c07c786b27f
Juan <juanes-x@hotmail.com>
2019-07-24 21:59:54 -0500

    Quinto commit para Heroku, se corrige error en el wsgi.py

--- Ciudades/wsgi.py
+++ Ciudades/wsgi.py
@@ -15,5 +15,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ciudades.settings')
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+import WhiteNoise
+application = WhiteNoise(application)
